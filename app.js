5e32cda83ce4f50ecbd1bbb8bfb48e6db17ab49d
a.mihnov <a.mihnov@anlax.org>
2023-08-31 20:32:41 +0300

    active buttons

--- app.js
+++ app.js
@@ -11,6 +11,25 @@ function getRandomFloat(min , max){
 let pointPlayer = 0;
 let pointComputer = 0;
 
+// document.getElementById('gameBtn').addEventListener('click', playRound);
+// let button = document.getElementById('gameBtn').addEventListener('click', playerChoiceFn)
+let buttons = document.querySelectorAll('.gameBtn');
+buttons.forEach(buttons => {
+    buttons.addEventListener('click', (event) => {
+        event.target.classList.add('active');
+        return buttons.textContent;
+    });
+});
+
+
+function playerChoiceFn(){
+    const buttonContent = document.getElementById('gameBtn'); 
+    const playerSelection = buttonContent.getAttribute('name');
+    console.log(playerSelection)
+}
+
+
+
 // one round game
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === "Rock" && computerSelection === "Scissors"){
@@ -47,16 +66,26 @@ function playRound(playerSelection, computerSelection) {
 }
 
 
-// 5 round game
-function game() {
-    for (let i = 1; i <= 5; i++) {
-        let enterSelect = prompt("Make your choice! (Rock, Scissors, or Paper)").trim();
-        const playerSelection = enterSelect.charAt(0).toUpperCase() + enterSelect.slice(1).toLowerCase();
-        const computerSelection = getComputerChoice(arr);
-        console.log(playRound(playerSelection, computerSelection));
-    }
-}
-game();
+// // 5 round game
+// function game() {
+//     for (let i = 1; i <= 5; i++) {
+//         let enterSelect = prompt("Make your choice! (Rock, Scissors, or Paper)").trim();
+//         const playerSelection = enterSelect.charAt(0).toUpperCase() + enterSelect.slice(1).toLowerCase();
+//         const computerSelection = getComputerChoice(arr);
+//         console.log(playRound(playerSelection, computerSelection));
+//     }
+// }
+// game();
+const computerSelection = getComputerChoice(arr);
+
+
+
+// function playerChoice(){
+//     let enterSelect = prompt("Make your choice! (Rock, Scissors, or Paper)").trim();
+//         const playerSelection = enterSelect.charAt(0).toUpperCase() + enterSelect.slice(1).toLowerCase();
+//         return playerSelection;
+// }
+
 
 
 function showPoint(pointPlayer, pointComputer){
